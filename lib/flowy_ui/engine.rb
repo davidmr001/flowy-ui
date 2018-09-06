284f58a76e13cc7862b01b8804da51c4352137e8
Pedro Assuncao <assuncas@gmail.com>
2018-09-06 18:27:45 +0100

    Using another namespace syntax to try to fix gem inclusion from github

--- lib/flowy_ui/engine.rb
+++ lib/flowy_ui/engine.rb
@@ -4,14 +4,18 @@ require 'react-rails'
 require 'jquery-rails'
 require 'active_model_serializers'
 
-#
-# Workflow UI rails engine
-#
-class FlowyUi::Engine < ::Rails::Engine
-  isolate_namespace FlowyUi
+# rubocop:disable Style/ClassAndModuleChildren
+module FlowyUi
+  #
+  # Workflow UI rails engine
+  #
+  class Engine < ::Rails::Engine
+    isolate_namespace FlowyUi
 
-  # Testing framework
-  config.generators do |g|
-    g.test_framework :rspec
+    # Testing framework
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
+# rubocop:enable Style/ClassAndModuleChildren
